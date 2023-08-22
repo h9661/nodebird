1404d9ed5c0719bf6bb362f54d2b704b1be17e90
h9661 <guscksdn123@gmail.com>
2023-08-22 15:13:27 +0900

    req.user render할때 정보 뺐음. res.locals에 이미 그 정보가 있기 때문에.

--- controllers/page.js
+++ controllers/page.js
@@ -3,11 +3,11 @@ const User = require("../models/user");
 const Hashtag = require("../models/hashtag");
 
 function renderProfile(req, res) {
-    res.render("profile", { title: "내 정보 - NodeBird", user: req.user });
+    res.render("profile", { title: "내 정보 - NodeBird" });
 }
 
 function renderJoin(req, res) {
-    res.render("join", { title: "회원가입 - NodeBird", user: req.user });
+    res.render("join", { title: "회원가입 - NodeBird" });
 }
 
 async function renderMain(req, res, next) {
@@ -22,7 +22,6 @@ async function renderMain(req, res, next) {
     res.render("main", {
         title: "NodeBird",
         twits: posts,
-        user: req.user,
     });
 }
 
