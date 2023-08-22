4bdaba98457ddd9befa20c3d62a5a7162a5286d3
h9661 <guscksdn123@gmail.com>
2023-08-22 22:02:05 +0900

    언팔로우 기능 추가

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,23 @@
 exports.follow = async (req, res, next) => {
     try {
         const user = req.user;
-        console.log(user);
         if (user) {
             await user.addFollowing(req.params.userId);
+            return res.send("success");
+        } else {
+            res.status(404).send("no user");
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
+exports.unfollow = async (req, res, next) => {
+    try {
+        const user = req.user;
+        if (user) {
+            await user.removeFollowing(req.params.userId);
             return res.send("success");
         } else {
             res.status(404).send("no user");
